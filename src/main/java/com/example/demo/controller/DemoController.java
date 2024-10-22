e5b5c465935c049075e5dc9a06fec9dff7c23ad2
Tam Nguyen <tamnc@imt-soft.com>
2024-10-22 23:03:29 +0700

    add

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -3,15 +3,20 @@ package com.example.demo.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 	
+	
+	private static final Logger log = LoggerFactory.getLogger(DemoController.class);
+ 
 	@GetMapping("/demo")
 	public List<String> getValue(){
-		System.out.println("show error");
+		log.info("show error");
 		return Arrays.asList("Demo");
 	}
 
