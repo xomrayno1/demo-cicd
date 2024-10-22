bfe47bf1d7cfc5e4ed74b0cad04dedf57fb10d17
Tam Nguyen <tamnc@imt-soft.com>
2024-10-22 22:59:17 +0700

    add

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	@GetMapping("/demo")
+	public List<String> getValue(){
+		System.out.println("show error");
+		return Arrays.asList("Demo");
+	}
+
+}
